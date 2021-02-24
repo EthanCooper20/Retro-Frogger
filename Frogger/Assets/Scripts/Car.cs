365f9d32880f91fa6d669db303552818dfa4caeb
EthanCooper20 <escoope77@gmail.com>
2021-02-24 10:16:29 -0600

    Add Car and Car Speed

---
+++ Frogger/Assets/Scripts/Car.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Car : MonoBehaviour {
+
+    public Rigidbody2D rb;
+
+    public float speed = 1f;
+
+    void FixedUpdate () {
+
+        Vector2 forward = new Vector2(transform.right.x, transform.right.y);
+        rb.MovePosition(rb.position + forward * Time.fixedDeltaTime * speed);
+    }
+}
