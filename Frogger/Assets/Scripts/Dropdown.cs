77b2e69481eecee52e8f8abe1809ae4ad95b2bd3
EthanCooper20 <escoope77@gmail.com>
2021-02-25 22:57:58 -0600

    Halfway Done with Intro Scene

---
+++ Frogger/Assets/Scripts/Dropdown.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dropdown : MonoBehaviour {
+
+    public TextMeshProUGUI output;
+
+    public void HandleInputData(int val)
+    {
+        if (val == 0)
+        {
+            output.text = "1";
+        }
+        if (val == 1)
+        {
+            output.text = "2";
+        }
+        if (val == 2)
+        {
+            output.text = "3";
+        }
+    }
+}
+
