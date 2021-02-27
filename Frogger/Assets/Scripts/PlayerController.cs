8a51a8aee1f025f8e9724c4607e79e89d341b5ac
EthanCooper20 <escoope77@gmail.com>
2021-02-27 17:41:11 -0600

    Limit Player Movement

---
+++ Frogger/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public const float MAX_SPEED = 5.0f;
+
+    private float halfPlayerSizeX;
+
+    void Start()
+    {
+        halfPlayerSizeX = GetComponent<SpriteRenderer>().bounds.size.x / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * MAX_SPEED * Time.deltaTime, 0.0f, 0.0f);
+        clampPlayerMovement();
+    }
+
+    void clampPlayerMovement()
+    {
+        Vector3 position = transform.position;
+
+        float distance = transform.position.z - Camera.main.transform.position.z;
+
+        float leftBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance)).x + halfPlayerSizeX;
+        float rightBorder = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance)).x - halfPlayerSizeX;
+
+        position.x = Mathf.Clamp(position.x, leftBorder, rightBorder);
+        transform.position = position;
+    }
+}
