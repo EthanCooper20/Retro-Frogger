67bce040670d7380b1e8fcbc668a435db8163934
EthanCooper20 <escoope77@gmail.com>
2021-02-27 22:55:17 -0600

    Last Modification

---
+++ Frogger/Assets/Scripts/GlobalControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalControl : MonoBehaviour
+{
+    public static GlobalControl Instance;
+
+    public float NameInput;
+    public float Lives;
+    public float GameSpeed;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        NameInput = GlobalControl.Instance.NameInput;
+        Lives = GlobalControl.Instance.Lives;
+        GameSpeed = GlobalControl.Instance.GameSpeed;
+    }
+}
